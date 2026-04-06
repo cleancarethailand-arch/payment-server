4beccd51af2915ecbbac5daa6b10a848fbad8273
cleancarethailand-arch <cleancarethailand@gmail.com>
2026-04-06 07:38:37 +0700

    Create index.js
    initial version of payment API

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let payments = {};
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/create-payment", (req, res) => {
+  const id = Math.random().toString(36).substring(7);
+  payments[id] = "pending";
+
+  res.json({
+    id: id,
+    qr: `https://your-app.onrender.com/pay?id=${id}`
+  });
+});
+
+app.get("/pay", (req, res) => {
+  const id = req.query.id;
+  payments[id] = "paid";
+  res.send("Payment Success");
+});
+
+app.get("/check-payment", (req, res) => {
+  const id = req.query.id;
+  res.send(payments[id] || "notfound");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
